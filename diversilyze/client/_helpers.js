6b038dbd461e32a19b082c10cc9f4bd1a6b4caa5
Andreas Rolén <andreasrolen93@gmail.com>
2014-11-23 15:21:55 +0100

    Mixed some errors

--- diversilyze/client/_helpers.js
+++ diversilyze/client/_helpers.js
@@ -20,21 +20,21 @@ Helpers = {
   checkDiversity: function(tracks, playlistID) {
     var promises = tracks.items.map(function(track) {
       return Spotify.getArtist(track.track.artists[0].id).then(function(artist) {
-        Spotify.getArtistRelatedArtists(artist.id).then(function(relatedArtist) {
-          RelatedArtistData.insert({playlistID: playlistID, artists: artist.id, genres: artist.genres});
-        })
         return artist.genres
       })
     })
     console.log(promises)
     return Promise.all(promises).then(function(genres) {
+      console.log(genres)
       var unique = _.chain(genres).flatten().uniq().value()
       var total = _.chain(genres).flatten().value()
+      console.log("unique: ", unique)
+      console.log("total", total)
       var diversity = unique.length/total.length;
       unique.forEach(function(genre) {
         GenresData.insert({playlistID: playlistID, genre: genre})
       })
-      
+      console.log(diversity)
       Diversity.insert({playlistID: playlistID, diversity: diversity});
       Session.set('currentPlaylist', playlistID)
       return diversity;
